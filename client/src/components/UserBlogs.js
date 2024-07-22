e5c3404ddfc870294f589b12eb2dc97f81260d7f
Ritik Sharma <103498410+123RitikSharma@users.noreply.github.com>
2024-07-22 19:46:05 +0530

    Update UserBlogs.js

--- client/src/components/UserBlogs.js
+++ client/src/components/UserBlogs.js
@@ -7,7 +7,7 @@ function UserBlogs() {
   const id = localStorage.getItem("userId");
   const sendRequest = async () => {
     const res = await axios
-      .get(`https://blog-website-api-gs8f.onrender.com${id}`)
+      .get(`http://localhost:8000/api/blog/user/${id}`)
       .catch((err) => console.log(err));
     const data = await res.data;
     return data;
